9d34689eb9cc7a997f6873ac32f44425ae9ea50c
Vinícius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2024-09-20 00:39:26 -0300

    test: Implementa o que foi adicionado na entidade de livros para o teste

--- backend/src/test/java/com/library/application/models/BookTest.java
+++ backend/src/test/java/com/library/application/models/BookTest.java
@@ -1,13 +1,19 @@
 package com.library.application.models;
 
 import com.library.application.StartDatabaseTest;
+import com.library.util.errors.handlers.ServerErrorHttpHandler;
 import io.ebean.Database;
+import org.joda.time.LocalDateTime;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class BookTest {
+    private static final Logger logger = LoggerFactory.getLogger(BookTest.class);
+
     private static Database database;
 
     /**
@@ -26,14 +32,23 @@ class BookTest {
         Book book = new Book();
         book.setIdBook(1);
         book.setDsBookName("Manifesto Comunista");
+        book.setDsAuthorName("Karl Marx");
+        book.setDsSummary("Livro sobre ideia econômica");
+        book.setDsReleaseDate(LocalDateTime.now());
 
         database.save(book);
+        logger.info("Objeto do livro instanciado e salvo dentro do banco com sucesso!");
 
         Book foundBook = database.find(Book.class, 1);
+        logger.info("Livro encontrado dentro do banco de dados!");
 
         assertThat(foundBook.getIdBook()).isEqualTo(book.getIdBook());
         assertThat(foundBook.getDsBookName()).isEqualTo(book.getDsBookName());
+        assertThat(foundBook.getDsAuthorName()).isEqualTo(book.getDsAuthorName());
+        assertThat(foundBook.getDsSummary()).isEqualTo(book.getDsSummary());
+        assertThat(foundBook.getDsReleaseDate()).isEqualTo(book.getDsReleaseDate());
 
         database.delete(book);
+        logger.info("Livro deletado do banco com sucesso!");
     }
 }
