a4a58fb08b0cca5186f145502a1ae50224029e51
Vinícius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2024-09-20 00:37:35 -0300

    feat: Adiciona novas colunas a entidade de livro, colunas para descrição de dados do livros

--- backend/src/main/java/com/library/application/models/Book.java
+++ backend/src/main/java/com/library/application/models/Book.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.joda.time.LocalDateTime;
 
 @Data
 @Entity
@@ -16,6 +17,15 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idBook;
 
-    @Column(name = "DS_BOOK_NAME")
+    @Column(name = "DS_BOOK_NAME", nullable = false, length = 150)
     private String dsBookName;
+
+    @Column(name = "DS_AUTHOR_NAME", nullable = false, length = 100)
+    private String dsAuthorName;
+
+    @Column(name = "DS_RELEASE_DATE")
+    private LocalDateTime dsReleaseDate;
+
+    @Column(name = "DS_SUMMARY", columnDefinition = "TEXT", length = 500)
+    private String dsSummary;
 }
