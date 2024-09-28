dc6eccf4353f6c2cb5f348130f0545e58b70865e
Vinícius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2024-09-27 21:51:41 -0300

    [fix]: Corrijindo commit passado

---
+++ backend/src/main/java/com/library/application/dto/responses/BookResponse.java
@@ -0,0 +1,32 @@
+package com.library.application.dto.responses;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.joda.time.LocalDateTime;
+
+
+public record BookResponse(
+        @JsonProperty("_id_book")
+        Long idBook,
+
+        @JsonProperty("ds_quantity_books")
+        Integer dsQuantityBooks,
+
+        @JsonProperty("_data_book")
+        DataBook dataBook
+) {
+}
+
+record DataBook(
+        @JsonProperty("ds_book_name")
+        String dsBookName,
+
+        @JsonProperty("ds_author_name")
+        String dsAuthorName,
+
+        @JsonProperty("ds_release_date")
+        LocalDateTime dsReleaseDate,
+
+        @JsonProperty("ds_summary")
+        String dsSummary
+) {
+}
