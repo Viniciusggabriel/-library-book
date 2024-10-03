4f1b87a5ef350033aaadf7260fcf215404898265
Vinícius Gabriel Pereira Leitão <vinicius.gabriel.p.leitao@gmail.com>
2024-10-03 11:04:14 -0300

    [refactor]: Altera o DTO de request de livros
    O request de livros possuia um erro ao requisitar a data de lançamento do livro, após isso foi adicionado uma anotação para verificar o Pattern

--- backend/src/main/java/com/library/application/dto/requests/BookRequest.java
+++ backend/src/main/java/com/library/application/dto/requests/BookRequest.java
@@ -1,5 +1,6 @@
 package com.library.application.dto.requests;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.ZonedDateTime;
@@ -11,6 +12,7 @@ public record BookRequest(
         @JsonProperty("ds_author_name")
         String dsAuthorName,
 
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC")
         @JsonProperty("ds_release_date")
         ZonedDateTime dsReleaseDate,
 
