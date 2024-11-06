5e9dd2638c592593fa9d82691dfbb7cfb3e6284b
Vinicius-Gabriel-P-Leitao <vinicius.gabriel.p.leitao@gmail.com>
2024-11-06 01:18:33 -0300

    [feat]: Implementa a geração de token
    Geração de tokens e retorno de tokens para o usuário está funcionando corretamente, será feito testes e validações

--- backend/src/main/java/com/library/application/controllers/users_in_library/AuthenticateUserInLibraryController.java
+++ backend/src/main/java/com/library/application/controllers/users_in_library/AuthenticateUserInLibraryController.java
@@ -1,10 +1,12 @@
 package com.library.application.controllers.users_in_library;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.library.application.dto.requests.UserInLibraryRequest;
 import com.library.application.services.AuthenticateUserInLibraryService;
 import com.library.util.errors.exceptions.InputOutputDataException;
 import com.library.util.errors.exceptions.InvalidRequestPayloadException;
 import com.library.util.utilitarian.ManipulateJsonObject;
+import com.nimbusds.jose.JOSEException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,6 +15,11 @@ import org.eclipse.jetty.http.HttpStatus;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.library.infra.security.JwtGenerateService.generateToken;
+import static com.library.util.utilitarian.ManipulateJsonObject.generateJson;
 
 public class AuthenticateUserInLibraryController extends HttpServlet {
     private final AuthenticateUserInLibraryService authenticateUserInLibraryService;
@@ -43,8 +50,17 @@ public class AuthenticateUserInLibraryController extends HttpServlet {
             throw new InvalidRequestPayloadException("A sua autenticação falhou!", HttpStatus.BAD_REQUEST_400);
         }
 
-        resp.setStatus(HttpServletResponse.SC_CREATED);
-        resp.getWriter().write("Sucesso ao autenticar o usuário!");
+        try {
+            String token = generateToken(userInLibraryRequest.dsUserName());
+            Map<String, String> map = new HashMap<>();
+            map.put("token", token);
+
+            resp.setStatus(HttpServletResponse.SC_CREATED);
+            resp.getWriter().write(generateJson(map));
+        } catch (JOSEException exception) {
+            throw new RuntimeException(exception);
+        }
+
     }
 
 }
